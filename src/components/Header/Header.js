15a06566e6fc276c9afb6581cd26acd2eb9c09c7
lukaszkus <kusiu@vp.pl>
2022-09-06 14:47:33 +0200

    Topbar, Header, Portal styles added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,8 +2,19 @@ import React from "react";
 
 import "./Header.scss";
 
+import Portal from "../Portal/Portal";
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="header-wrapper">
+      <div className="container">
+        <header className="header">
+          <h1 className="header__title">Rick and Morty cool title.</h1>
+          <Portal />
+        </header>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
--- src/components/Topbar/Topbar.js
+++ src/components/Topbar/Topbar.js
@@ -1,9 +1,31 @@
 import React from "react";
+import Logo from "../../assets/logo.svg";
 
 import "./Topbar.scss";
 
 const Topbar = () => {
-  return <div>Topbar</div>;
+  return (
+    <div className="topbar-wrapper">
+      <div className="container">
+        <div className="topbar">
+          <img className="logo" src={Logo} alt="Rick and Morty logo" />
+          <nav className="nav">
+            <ul className="nav__list">
+              <li className="nav__item">
+                <span>01.</span> Characters
+              </li>
+              <li className="nav__item">
+                <span>02.</span> Episodes
+              </li>
+              <li className="nav__item">
+                <span>03.</span> Locations
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Topbar;
